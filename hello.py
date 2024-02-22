fb19f8d7540cb6387f4809858d971de61b4120b5
nicolas freitas <PT3019144@IFSPROOT.PTB>
2024-02-22 20:10:11 -0300

    First commit

--- hello.py
+++ hello.py
@@ -1,6 +1,19 @@
 # A very simple Flask Hello World app for you to get started with...
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
+
+
 @app.route('/')
-def hello_world():
-    return '<p>Hello from Flask!</p><table><tr><td><b>Aluno:</b></td><td>Nicolas Freitas Silveira</td></tr><tr><td><b>Prontuário:</b></td><td>PT3019144</td></tr></table>'
+def index():
+
+    return '<h1>Avaliação contínua: Aula 030</h1><table><tr><td><a href="/">Home</a></td></tr><tr><td><b><a href="">Identificação</a></b></td><td><a href="/contextorequisicao">Contexto da requisição</a></td></tr></table>'
+
+
+@app.route('/contextorequisicao')
+def contexto_requisicao():
+
+    user_agent = request.headers.get('User-Agent')
+    remote_ip = request.remote_addr
+    host = request.host
+
+    return '<h1>Avaliação contínua: Aula 030</h1><h2>Seu navegador é: {{ user_agent }}</h2><h2>O IP do computador remoto é: {{ remote_ip }}</h2><h2>O host da aplicação é: {{ host }}</h2><a href="/">Voltar</a>'
